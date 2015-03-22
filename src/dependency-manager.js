3236ce6e9b187f0d9e495d5638be7775590948b0
helios1138 <revan.den@gmail.com>
2015-03-22 18:12:08 +0200

    es5

--- src/dependency-manager.js
+++ src/dependency-manager.js
@@ -1,52 +1,54 @@
-import {Dependency} from './dependency';
+'use strict';
 
-export class DependencyManager {
-  constructor() {
-    this._dependencies = {};
-  }
+var Dependency = require('./dependency').Dependency;
 
-  set(name, type, value) {
-    if (typeof this._dependencies[name] === 'undefined') {
-      this._dependencies[name] = new Dependency(this);
-    }
-
-    this._dependencies[name].provide(type, value);
-  }
-
-  get(name) {
-    if (typeof this._dependencies[name] === 'undefined') {
-      this._dependencies[name] = new Dependency(this);
-    }
-
-    return this._dependencies[name];
-  }
-
-  resolve(dependencyNames) {
-    return Array.isArray(dependencyNames) ?
-      this._resolveAsArray(dependencyNames) :
-      this._resolveAsObject(dependencyNames);
-  }
-
-  _resolveAsArray(dependencyNames) {
-    return Promise
-      .all(dependencyNames
-        .map((name) => this.get(name).getPromise()
-      ));
-  }
-
-  _resolveAsObject(dependencyNames) {
-    var names = Object.keys(dependencyNames);
-
-    return this
-      ._resolveAsArray(names)
-      .then((dependencies) => {
-        var obj = {};
-
-        dependencies.forEach((dependency, i) => {
-          obj[names[i]] = dependency;
-        });
-
-        return obj;
-      });
-  }
+function DependencyManager() {
+  this._dependencies = {};
 }
+
+DependencyManager.prototype.set = function (name, type, value) {
+  if (typeof this._dependencies[name] === 'undefined') {
+    this._dependencies[name] = new Dependency(this);
+  }
+
+  this._dependencies[name].provide(type, value);
+};
+
+DependencyManager.prototype.get = function (name) {
+  if (typeof this._dependencies[name] === 'undefined') {
+    this._dependencies[name] = new Dependency(this);
+  }
+
+  return this._dependencies[name];
+};
+
+DependencyManager.prototype.resolve = function (dependencyNames) {
+  return Array.isArray(dependencyNames) ?
+    this._resolveAsArray(dependencyNames) :
+    this._resolveAsObject(dependencyNames);
+};
+
+DependencyManager.prototype._resolveAsArray = function (dependencyNames) {
+  return Promise
+    .all(dependencyNames
+      .map(function (name) {return this.get(name).getPromise();}.bind(this)
+    ));
+};
+
+DependencyManager.prototype._resolveAsObject = function (dependencyNames) {
+  var names = Object.keys(dependencyNames);
+
+  return this
+    ._resolveAsArray(names)
+    .then(function (dependencies) {
+      var obj = {};
+
+      dependencies.forEach(function (dependency, i) {
+        obj[names[i]] = dependency;
+      }.bind(this));
+
+      return obj;
+    }.bind(this));
+};
+
+module.exports = { DependencyManager: DependencyManager };
