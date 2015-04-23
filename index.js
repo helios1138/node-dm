2dd41b9b74688785efb497d65fe3ee765943c10b
helios1138 <revan.den@gmail.com>
2015-04-23 15:36:34 +0300

    2.0.5

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Manager = require('./src/manager').Manager;
-var dm = new Manager();
+var Container = require('./src/container').Container;
+var dm = new Manager(new Container());
 
 module.exports = dm;
